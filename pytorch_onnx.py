14c80c94f4f198201e34f9cafd810f10afea487c
david <automorphicnew@gmail.com>
2021-11-30 10:28:55 +0900

    delete onnx transform

--- pytorch_onnx.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.onnx
-from utils import mobilenet
-model_file_dir = "model_v3/tomato_ripeness_56_.pt"
-
-model = mobilenet(32, 1, 37, 256)
-model.load_state_dict(torch.load(model_file_dir))
-dummy_input = torch.randn(1, 1, 32, 100)
-torch.onnx.export(model, dummy_input, "crnn.onnx", verbose=True)
